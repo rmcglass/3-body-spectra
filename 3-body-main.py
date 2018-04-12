2a837ae7439d85159597626f10845c2cf0cc93a2
Riley McGlasson <rmcglass@macalester.edu>
2018-04-12 12:56:11 -0500

    added py file

---
+++ 3-body-main.py
@@ -0,0 +1,16 @@
+##### initialize variables
+maxTime = 50
+# make masses
+m_1 = 10
+m_2 = 10
+m_3 = 10
+#initial velocity vectors
+vi_1 = [50,0,0]
+vi_2 = [0,50,0]
+vi_3 = [0,0,50]
+#intitial positions
+posi_1 = [100,0,0]
+posi_2 = [0,100,0]
+posi_3 = [0,0,100]
+
+
